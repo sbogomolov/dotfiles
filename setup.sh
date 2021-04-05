4a3e9f1155ad78be2350144e23b457090176521e
Sergii Bogomolov <sergii@bogomolov.io>
2021-04-05 20:33:15 +0200

    Start gsd-xsettings

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ sudo_create_symlink() {
 
 
 # Install required RPM packages
-PACKAGES=(git compton adwaita-gtk2-theme ImageMagick pulseaudio-utils pavucontrol pasystray openssh-askpass fzf ripgrep xset xrdb xss-lock)
+PACKAGES=(git compton adwaita-gtk2-theme ImageMagick pulseaudio-utils pavucontrol pasystray openssh-askpass fzf ripgrep xset xrdb xss-lock gnome-settings-daemon)
 echo "Installing packages: ${PACKAGES[@]}"
 sudo dnf -y install "${PACKAGES[@]}"
 
