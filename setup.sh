ab953da6e7b38a2cfcbfef9b76b7a54ca3d8491a
Sergii Bogomolov <sergii@bogomolov.io>
2021-06-23 13:17:12 +0200

    Use SHA256 checksum instead of MD5

--- setup.sh
+++ setup.sh
@@ -50,8 +50,8 @@ copy() {
 
     if [ -e "$dst" ]; then
         echo "- File or directory already exists"
-        md5_old="$(md5sum "$dst" | awk '{ print $1 }')"
-        md5_new="$(md5sum "$src" | awk '{ print $1 }')"
+        md5_old="$(sha256sum "$dst" | awk '{ print $1 }')"
+        md5_new="$(sha256sum "$src" | awk '{ print $1 }')"
         if [ "$md5_old" = "$md5_new" ]; then
             echo "- Files are identical, skipping the copy"
             return
